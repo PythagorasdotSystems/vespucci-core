902074d3e3b80cb98d7c565289d49906d0be7db8
Christoforou Manolis <christoforou712@gmail.com>
2018-09-11 11:59:00 +0000

    fta_features add coinmetrics db

--- db/db.py
+++ db/db.py
@@ -2,12 +2,11 @@ import pyodbc
 
 class DB:
     def __init__(self):
-        self.server = ''
-        self.database = ''
-        self.usrname = ''
-        self.password = ''
-
-        self.driver=''
+        self.__server = ''
+        self.__database = ''
+        self.__usrname = ''
+        self.__password = ''
+        self.__driver= ''
 
         self.cnxn = None
 
@@ -16,4 +15,4 @@ class DB:
         self.cnxn = pyodbc.connect('DRIVER=' + self.__driver + ';SERVER=' + self.__server + ';PORT=1443;DATABASE=' + self.__database + ';UID=' + self.__usrname + ';PWD=' + self.__password)
 
     def disconnect(self):
-        self.cnxn.close() 
+        self.cnxn.close()
--- fta_features.py
+++ fta_features.py
@@ -4,12 +4,16 @@ from coinmetrics.coinmetrics import CoinMetricsAPI
 from fta.fta_coin import social_features
 from fta.fta_coin import block_features
 
+from db import DB
+
 import datetime
 import time
 
 def fta_features(num_coins = 10):
     cg = CoinGeckoAPI()
     cm = CoinMetricsAPI()
+
+    db = DB()
 
     # returns top 50 by default
     coins_ranked = cg.get_coins(order='market_cap_desc')
@@ -51,17 +55,62 @@ def fta_features(num_coins = 10):
     coins_block_features = block_features(coin_symbols_ranked)
     coins_social_features = social_features(coin_symbols_ranked)
 
-    return coins_block_features, coins_social_features
+    return cm_coins_features, coins_block_features, coins_social_features
 
-def db_coinmetrics(coin_features):
-    pass
+def db_coinmetrics(coin_features, db = None):
+    if not db:
+        db = DB()
+    db.connect()
+
+    r = {}
+    # fix Dic to format Dic[coin][timestamp][feature] instead of Dic[coin][feature][[timestamp,feature_value],[timestamp2, feature_value]]
+    for coin in coin_features:
+        r[coin] = {}
+        for feat in coin_features[coin]:
+            for feat_ts in coin_features[coin][feat]:
+                if feat_ts[0] not in r[coin].keys():
+                    r[coin][feat_ts[0]] = {}
+                r[coin][feat_ts[0]][feat] = feat_ts[1]
+        #print(coin)
+        #print(coin_features[coin])
+        #print(coin_features[coin].keys())
+
+    for coin in r:
+        for ts in r[coin]:
+            Values = []
+            db_query = 'INSERT INTO FtaCoinmetrics ('
+
+            db_query += ' Symbol'
+            Values.append(coin)
+
+            db_query += ', CM_Timestamp'
+            Values.append(datetime.datetime.fromtimestamp(ts))
+
+            #db_query += ',' + ','.join(r[coin][ts].keys())
+            db_query += ',' + ','.join(list(map(( lambda x: '[' + x + ']'), (r[coin][ts].keys()))))
+            Values.extend(list(r[coin][ts].values()))
+
+            db_query += ') VALUES '
+            #list(map(( lambda x: '[' + x + ']'), list(r[coin][ts].keys())))
+            db_query += '(' + ','.join(['?' for x in r[coin][ts].keys()]) + ',?,? ' + ')'
+
+
+            #print(db_query)
+            #print(Values)
+            db.cnxn.execute(db_query, tuple(Values))
+            db.cnxn.commit()
+
+    db.disconnect()
+
+    return r
+
 
 if __name__ == "__main__":
-    while(1):
-        coins_block_features, coins_social_features = fta_features()
-        t0 = datetime.datetime.fromtimestamp(coins_block_features['btc']['LastBlockExplorerUpdateTS'])
-        print(t0)
-        t1 = t0 + datetime.timedelta(0,(30*60)+5)
-        print('sleep for ' + str((t1-t0).seconds) + ' seconds')
-        time.sleep((t1-t0).seconds)
+#    while(1):
+    cm_coins_features, coins_block_features, coins_social_features = fta_features()
+#    t0 = datetime.datetime.fromtimestamp(coins_block_features['btc']['LastBlockExplorerUpdateTS'])
+#    print(t0)
+#    t1 = t0 + datetime.timedelta(0,(30*60)+5)
+#    print('sleep for ' + str((t1-t0).seconds) + ' seconds')
+#    time.sleep((t1-t0).seconds)
 
