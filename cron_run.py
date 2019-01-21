1c88c065a607fcbfa6ccd2d45ca5e52f1e666d0e
Christoforou Manolis <emchristoforou@gmail.com>
2019-01-21 13:56:06 +0000

    update cron_run config & pass it as argument (-c or --config)

--- cron_run.py
+++ cron_run.py
@@ -4,11 +4,28 @@ import v_ta
 import v_fta
 import v_score
 
-config = utils.tools.ConfigFileParser('./config.yml')
+import sys, getopt
 
+# load config file
+configfile='./config.yml'
+try:
+    opts, args = getopt.getopt(sys.argv[1:],'c:',['config='])
+except getopt.GetoptError:
+    print('cron_run.py -c <configfile>')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ('-c','--config'): configfile=arg
+
+# set config file
+utils.tools.set_config(configfile)
+
+# create logger
 logger = utils.logger_default('fta_&_scores_cron', '../fta_scores_cron.log')
 
 logger.info('Starting (Update TA & FTA feats and compute score)')
+
+# update TA, FTA and compute new scores
 
 # TA features
 logger.info('Update TA feats')
